123d6a8e976ae014c1c22ec45325276a0f1b1486
squareorc <harsh.bhogal7@gmail.com>
2025-03-22 01:01:18 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Alert from "./components/Alert";
+function App() {
+//  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+//
+//  const handleSelectItem = (item: string) => {
+//    console.log(item);
+//  }
+
+  return (
+    <div>
+     <Alert>
+      Hello <span>World</span>
+     </Alert>
+    </div>
+  );
+}
+export default App;
